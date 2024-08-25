770db77b57d436fb0cfdef2230d515d115ff794b
Arpana Kumari <kashyaparpana203@gmail.com>
2024-08-25 18:27:29 +0530

    about repo

--- datastructre-algorithms.py
+++ datastructre-algorithms.py
@@ -1,1 +1,3 @@
-#Question and solutions based on DSA.
+# Data Structures and Algorithms
+# Overview:
+# This repository contains a collection of problems and solutions related to data structures and algorithms. It is designed to help users practice and improve their problem-solving skills and understanding of fundamental concepts in computer science.
