5195b61f1d23285a9596ec50ba79c7af941bfdf4
Arpana Kumari <kashyaparpana203@gmail.com>
2024-08-31 16:16:01 +0530

    readme-updated

--- datastructre-algorithms.py
+++ datastructre-algorithms.py
@@ -1,3 +1,3 @@
 # Data Structures and Algorithms
 # Overview:
-# This repository contains a collection of problems and solutions related to data structures and algorithms. It is designed to help users practice and improve their problem-solving skills and understanding of fundamental concepts in computer science.
+# This repository contains a collection of problems and solutions related to data structures and algorithms. It is designed to help users practice and improve their problem-solving skills and understanding of fundamental concepts in problem solving.
