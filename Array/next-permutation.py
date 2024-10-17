6a0d6f0cc6b1ac88ab7b8e7ec3daf29c83596e97
Arpana Kumari <kashyaparpana203@gmail.com>
2024-10-17 22:12:36 +0530

    day54

--- Array/next-permutation.py
+++ Array/next-permutation.py
@@ -7,10 +7,47 @@
 nums = [1,2,3]
 # Output: [1,3,2]
 
-# Better using builtin function
+# Brute force
+nums = [1,2,3]
+def nextPermutation(nums):
+    n = len(nums)
+    result = []
+    
+    def helper(nums,start):
+        if start == n:
+            result.append(nums[:])
+            return
+        seen = set()
+        for i in range(start,n):
+            if nums[i] in seen:
+                continue
+            seen.add(nums[i])
+
+            nums[i],nums[start] = nums[start],nums[i]
+            helper(nums,start+1)
+            nums[i],nums[start] = nums[start],nums[i]
+
+    helper(nums,0)
+    result.sort()
+    
+    # Find the next permutation after `nums`
+    for i in range(len(result)):
+        if result[i] == nums:
+            # Return the next permutation, 
+            # or the first if `nums` is the last one
+            return result[(i + 1) % len(result)]
+    # If `nums` is not found, return None (or handle the case as needed)
+    return None
+    
+res = nextPermutation(nums)
+print(res)
+
+# TC = 0(N! * N)
+# SC = 0(N!)
+
+# Better using builtin function for C++ user
 
 # Optimal approach
-
 def nextPermutation(nums):
         """
         Do not return anything, modify nums in-place instead.
