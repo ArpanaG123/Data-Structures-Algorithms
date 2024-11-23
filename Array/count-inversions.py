d55da8557c453691d3df77e53e3c9b90b300c255
Arpana Kumari <kashyaparpana203@gmail.com>
2024-11-23 17:39:03 +0530

    day77

--- Array/count-inversions.py
+++ Array/count-inversions.py
@@ -1,6 +1,8 @@
 # Count Inversions
 # Link - https://www.geeksforgeeks.org/problems/inversion-of-array-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=inversion-of-array
 
+# Topic - Array, DP
+
 # Given an array of integers. Find the Inversion Count in the array.  Two elements arr[i] and arr[j] form an inversion if arr[i] > arr[j] and i < j.
 # Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If the array is already sorted then the inversion count is 0.
 # If an array is sorted in the reverse order then the inversion count is the maximum. 
