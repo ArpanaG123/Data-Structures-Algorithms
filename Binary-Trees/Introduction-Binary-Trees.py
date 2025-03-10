c7fdcab7208555eb8ee0b56f8c27814452c65252
Arpana Kumari <kashyaparpana203@gmail.com>
2025-03-10 21:16:19 +0530

    day106

---
+++ Binary-Trees/Introduction-Binary-Trees.py
@@ -0,0 +1,14 @@
+# Basic - Binary Trees
+# Binary Tree allow hirearchical organisation and structire of multilevel sequence.
+# Binary tree consist of root node,child node and leaf node.
+
+# Root node - the topmost node of binary tree from which all other node stem out.
+# Child node - It is directly connected to a parent node.In BT, a parent can have zero,one or teo children.
+# Leaf node - The node that do not have any children.
+
+# Types of Binary Tree
+# 1.Full BT
+# 2.Complete BT
+# 3.Perfect BT
+# 4.Balanced BT
+# 5.Degenerate Tree
